e7d2ca337007ccc09341c9771a4610a6597c1762
Acolytetech <slawaniya03@gmail.com>
2025-09-13 15:26:44 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    
+    turbo: false, // 👈 disable Turbopack
+  },
 };
 
 export default nextConfig;
