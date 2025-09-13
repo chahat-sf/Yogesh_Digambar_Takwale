30e00cdec38b199ccfe7988b4a7072515a422a9d
Acolytetech <slawaniya03@gmail.com>
2025-09-13 16:12:26 +0530

    j

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,28 @@ import Footer from "@/component/global/Footer";
 export const switzer = localFont({
   src: [
     {
-      path: "/fonts/Switzer-Regular.woff2",
+      path: "../public/fonts/Switzer-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "/fonts/Switzer-SemiBold.woff2",
+      path: "../public/fonts/Switzer-SemiBold.woff2",
       weight: "600",
+      style: "normal",
+    },
+     {
+      path: "../public/fonts/Switzer-SemiBold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+     {
+      path: "../public/fonts/Switzer-SemiBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+     {
+      path: "../public/fonts/Switzer-SemiBold.woff2",
+      weight: "900",
       style: "normal",
     },
   ],
@@ -25,12 +40,12 @@ export const switzer = localFont({
 export const involve = localFont({
   src: [
     {
-      path: "/fonts/Involve-SemiBold.woff2",
+      path: "../public/fonts/Involve-SemiBold.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "/fonts/Involve-SemiBold.woff2",
+      path: "../public/fonts/Involve-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
@@ -38,19 +53,19 @@ export const involve = localFont({
   variable: "--font-involve",
 });
 
-// URW Gothic
-export const urwGothic = localFont({
-  src: "/fonts/URWGothicL-Demi.woff2",
-  weight: "600",
-  style: "normal",
-  variable: "--font-urw",
-});
+// // URW Gothic
+// export const urwGothic = localFont({
+//   src: "../public/fonts/URWGothicL-Demi.woff2",
+//   weight: "600",
+//   style: "normal",
+//   variable: "--font-urw",
+// });
 
-// Spaciaz (icon font)
-export const spaciaz = localFont({
-  src: "/fonts/spaciaz-icon-1.1.0.woff2",
-  variable: "--font-spaciaz",
-});
+// // Spaciaz (icon font)
+// export const spaciaz = localFont({
+//   src: "../public/fonts/spaciaz-icon-1.1.0.woff2",
+//   variable: "--font-spaciaz",
+// });
 
 export const metadata: Metadata = {
   title: "Yogesh Digambar Takwale",
@@ -65,12 +80,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${switzer.variable} ${involve.variable} ${urwGothic.variable} ${spaciaz.variable}`}
+      className={`${switzer.variable} ${involve.variable} `}
     >
-      <body className="antialiased">
+      <body  className=" antialiased  " >
         <Navbar />
-        <div className="z-[1] -mt-[50px]">{children}</div>
-        <Footer />
+        <div className="z-1 -mt-50">{children}</div>
+        <Footer/>
       </body>
     </html>
   );
