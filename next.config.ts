880e2a9886462b67f6cc7bbe26f108229a772151
Acolytetech <slawaniya03@gmail.com>
2025-09-20 12:52:13 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 // next.config.ts
 import type { NextConfig } from "next";
-import path from "path";
 
 // If you really need it (monorepo/multiple lockfiles):
 const nextConfig = {
   // point this at your repo/workspace root
-  outputFileTracingRoot: path.join(__dirname, ".."),
 } satisfies NextConfig;
 
 export default nextConfig;
