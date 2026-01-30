aff76f9840357d6cf5355044b85cf1680a48ca79
Work <CodeMode@Chahats-MacBook-Pro.local>
2026-01-30 13:46:12 +0530

    adding config for CI/CD

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 // If you really need it (monorepo/multiple lockfiles):
 const nextConfig = {
   // point this at your repo/workspace root
+    output: "standalone",
 } satisfies NextConfig;
 
 export default nextConfig;
